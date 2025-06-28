bd4c788288f83b477e0f54140b9ec871ede1ae58
dhruvjagtap <jagtapdhruv6@gmail.com>
2025-06-28 17:40:58 +0530

    added gesture handler for swipe delete

--- App.tsx
+++ App.tsx
@@ -8,14 +8,15 @@
 // import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <AppNavigator />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
